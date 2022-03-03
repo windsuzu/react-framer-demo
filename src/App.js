be89c4aa7f2b444f77ad4fa2f1b2708fd2d02a99
windsuzu <windsuzu@gmail.com>
2022-03-03 17:35:49 +0800

    add basic react functionalities

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Base from "./components/Base";
+import Toppings from "./components/Toppings";
+import Order from "./components/Order";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [pizza, setPizza] = useState({ base: "", toppings: [] });
+
+    const addBase = (base) => {
+        setPizza({ ...pizza, base });
+    };
+
+    const addTopping = (topping) => {
+        let newToppings;
+        if (!pizza.toppings.includes(topping)) {
+            newToppings = [...pizza.toppings, topping];
+        } else {
+            newToppings = pizza.toppings.filter((item) => item !== topping);
+        }
+        setPizza({ ...pizza, toppings: newToppings });
+    };
+
+    return (
+        <>
+            <Header />
+            <Routes>
+                <Route
+                    path="/base"
+                    element={<Base addBase={addBase} pizza={pizza} />}
+                />
+                <Route
+                    path="/toppings"
+                    element={<Toppings addTopping={addTopping} pizza={pizza} />}
+                />
+                <Route path="/order" element={<Order pizza={pizza} />} />
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
